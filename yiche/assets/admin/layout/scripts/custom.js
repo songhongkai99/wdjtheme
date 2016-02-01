87c64bc813af9ef7d003237c51d3819abc123422
songhongkai99 <875672364@qq.com>
2016-02-01 13:01:11 +0800

    ultis

--- yiche/assets/admin/layout/scripts/custom.js
+++ yiche/assets/admin/layout/scripts/custom.js
@@ -1,6 +1,6 @@
 var hkcustom = {
 
-    showKeyPress : function(evt) { //捕获键盘事件
+    showKeyPress: function (evt) { //捕获键盘事件
         var keynum;
         if (window.event) {
             keynum = evt.keyCode;
@@ -10,7 +10,7 @@ var hkcustom = {
         return this.checkSpecificKey(keynum);
     },
 
-    checkSpecificKey : function(keyCode) { //禁止输入特殊字符
+    checkSpecificKey: function (keyCode) { //禁止输入特殊字符
         var specialKey = "_|{}\/\\<>()^*\'\"+,";
         var realkey = String.fromCharCode(keyCode);
         var flg = false;
@@ -23,15 +23,15 @@ var hkcustom = {
         }
     },
 
-    preventSelect2_custominput : function() { //阻止select2 输入特殊字符
-        $(document).on("keypress", "[id^='s2id_']", function(e) {
+    preventSelect2_custominput: function () { //阻止select2 输入特殊字符
+        $(document).on("keypress", "[id^='s2id_']", function (e) {
             return hkcustom.showKeyPress(e);
         })
     },
 
-    initBootstrapModal : function() { //模态框编辑修改初始化,多个模态框请用,分隔
-        $(".modal").on("hidden.bs.modal", function() { // hidden 完全隐藏出发 防止因为溢出层导致的不必要触发
-            $("form", this).each(function() {
+    initBootstrapModal: function () { //模态框编辑修改初始化,多个模态框请用,分隔
+        $(".modal").on("hidden.bs.modal", function () { // hidden 完全隐藏出发 防止因为溢出层导致的不必要触发
+            $("form", this).each(function () {
                 $(this)[0].reset();
                 $("#id").val("");
                 $.fn.validate && $(this).validate().resetForm();
@@ -40,29 +40,28 @@ var hkcustom = {
         })
     },
 
-    handleIframeHeight : function () {
-        if($(".page-iframe").length){
+    handleIframeHeight: function () {
+        if ($(".page-iframe").length) {
             var e = $(window.top.document.getElementById("page-iframe"));
-            if(e.parent().height() > $(document).height())
-                e.css("height", e.parent().height()+"px");
+            if (e.parent().height() > $(document).height())
+                e.css("height", e.parent().height() + "px");
             else
-                e.css("height", ($(document).height() <=400 ? 400 : $(document).height())+"px");
+                e.css("height", ($(document).height() <= 400 ? 400 : $(document).height()) + "px");
         }
     },
 
-    handleTopMenubar : function () {
-        $("#topMenubar").children("li").on("click", function () {
+    handleTopMenubar: function () {
+        $("#topMenubar").on("click", "li", function () {
             $(this).siblings("li").removeClass("active").end().addClass("active");
             var $leftbar = $(".leftMenubar"),
-                $thisa   = $leftbar.eq($(this).index()).find("ul:first li:first a");
-            $leftbar.removeClass("active").addClass("hide").eq($(this).index()).removeClass("hide").find("ul:first li:first a").click();
-            ($thisa.attr("href") !="#" && $.trim($thisa.attr("href")) !="")
-            && $("#page-iframe").attr("src",$thisa.attr("href"));
+                $thisa = $leftbar.filter(".for-topmenubar-" + $(this).index()).first().find("ul:first li:first a");
+            $leftbar.removeClass("active").addClass("hide").filter(".for-topmenubar-" + $(this).index()).removeClass("hide").first().find("ul:first li:first a").click();
+            ($thisa.attr("href") != "#" && $.trim($thisa.attr("href")) != "" && $($thisa.attr("href")) != "javascript:;") && $("#page-iframe").attr("src", $thisa.attr("href"));
         })
     },
 
-    handleSidebarMenu : function () {
-        $(".page-sidebar-menu>li>ul>li>a").on("click", function () {
+    handleSidebarMenu: function () {
+        $(".page-sidebar-menu").on("click", ">li>ul>li>a", function () {
             $(".page-sidebar-menu>li").removeClass("active");
             $(this).closest("ul").closest("li").addClass("active");
             $(".page-sidebar-menu>li li").removeClass("active");
@@ -70,33 +69,33 @@ var hkcustom = {
         })
     },
 
-    handleDatePicker : function(){
+    handleDatePicker: function () {
         $().datepicker && $(".date-picker").datepicker({
             autoclose: true,
             format: "yyyy-mm-dd",
-            language:"zh-CN"
+            language: "zh-CN"
         });
     },
 
-    handleSelect2 : function () {
+    handleSelect2: function () {
         $().select2 && $(".select2").select2({
             minimumResultsForSearch: -1 //disable search option
         });
     },
 
-    handleModalPosition : function () {
-        $(".page-iframe").on("show.bs.modal",".modal", function () {
+    handleModalPosition: function () {
+        $(".page-iframe").on("show.bs.modal", ".modal", function () {
             var st = $(top).scrollTop();
             $(".modal-dialog", this).css({
-                "marginTop": (st > 30 ? st : st+30)+"px"
+                "marginTop": (st > 30 ? st : st + 30) + "px"
             })
         })
     },
 
-    handleValidateForm : function () {
-        if($.validator){
+    handleValidateForm: function () {
+        if ($.validator) {
             var form1 = $('form');
-            form1.each(function (i,e) {
+            form1.each(function (i, e) {
                 var error1 = $('.alert-danger', $(this));
                 var success1 = $('.alert-success', $(this));
                 $(this).validate({
@@ -105,8 +104,7 @@ var hkcustom = {
                     focusInvalid: true, // do not focus the last invalid input
 
                     ignore: "",  // validate all fields including form hidden input
-                    rules: {
-                    },
+                    rules: {},
 
                     invalidHandler: function (event, validator) { //display error alert on form submit
                         //alert("您的输入有误, 请您更正错误后重新提交.")
@@ -136,13 +134,13 @@ var hkcustom = {
 
     },
 
-    hanldeDataTableInit : function () {
-        $.fn.dataTable && ($.fn.dataTable.defaults.fnInitComplete = function(){
+    hanldeDataTableInit: function () {
+        $.fn.dataTable && ($.fn.dataTable.defaults.fnInitComplete = function () {
             //hkcustom.handleIframeHeight();
         })
     },
 
-    init : function () {
+    init: function () {
         this.preventSelect2_custominput();
         this.initBootstrapModal();
         this.handleTopMenubar();
